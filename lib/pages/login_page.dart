c85b42cc51f7559a3aba76e269b2c06237caa273
jamescardona11 <jjcardonoa@gmail.com>
2020-05-25 23:16:36 -0500

    Create ui/ux for landscape and portrait

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:signinfirebaseapp/pages/widgets/login_form.dart';
@@ -11,17 +12,26 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with AfterLayoutMixin {
   @override
   void initState() {
     super.initState();
-
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    bool isTablet = MediaQuery.of(context).size.shortestSide > 600.0;
+
+    if (!isTablet) {
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final Responsive responsive = new Responsive.of(context);
+
     return Scaffold(
       body: GestureDetector(
         //TODO when click in textfield singlechild scroll to top
@@ -29,22 +39,52 @@ class _LoginPageState extends State<LoginPage> {
           FocusScope.of(context).unfocus();
         },
         child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white,
-          child: SingleChildScrollView(
-            child: Container(
-              height: responsive.height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  WelcomeWidget(),
-                  LoginFormWidget(),
-                ],
-              ),
-            ),
-          ),
-        ),
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.white,
+            child: OrientationBuilder(
+              builder: (context, orientation) {
+                if (orientation == Orientation.portrait) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      height: responsive.height,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          WelcomeWidget(),
+                          LoginFormWidget(),
+                        ],
+                      ),
+                    ),
+                  );
+                } else {
+                  return Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: SingleChildScrollView(
+                          physics: NeverScrollableScrollPhysics(),
+                          child: Container(
+                            padding: EdgeInsets.only(left: 25),
+                            height: responsive.height,
+                            child: WelcomeWidget(),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Container(
+                            height: responsive.height,
+                            child: Center(
+                              child: LoginFormWidget(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+              },
+            )),
       ),
     );
   }
