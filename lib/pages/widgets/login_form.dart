ad85cd6f1fbceaa20c69633f38a48e8c54e7019c
jamescardona11 <jjcardonao@gmail.com>
2020-05-25 22:46:30 -0500

    UI/UX for login is done. Next step: - Add functionality - remove errors like overflow

--- lib/pages/widgets/login_form.dart
+++ lib/pages/widgets/login_form.dart
@@ -1,38 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:signinfirebaseapp/widgets/circle_button.dart';
 import 'package:signinfirebaseapp/widgets/rounded_button.dart';
 
 class LoginFormWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 330,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          _InputTextLogin(
-            iconPath: 'assets/icons/email.svg',
-            placeholder: 'Email address',
-          ),
-          SizedBox(height: 20),
-          _InputTextLogin(
-            iconPath: 'assets/icons/key.svg',
-            placeholder: 'Password',
-          ),
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerRight,
-            child: CupertinoButton(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: Text('Forgot password?', style: TextStyle(fontFamily: 'sans')),
+    return SafeArea(
+      top: false,
+      child: Container(
+        width: 330,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _InputTextLogin(
+              iconPath: 'assets/icons/email.svg',
+              placeholder: 'Email address',
+            ),
+            SizedBox(height: 20),
+            _InputTextLogin(
+              iconPath: 'assets/icons/key.svg',
+              placeholder: 'Password',
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerRight,
+              child: CupertinoButton(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Text('Forgot password?', style: TextStyle(fontFamily: 'sans')),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(height: 10),
+            RoundedButtonWidget(
+              label: 'Sign in',
               onPressed: () {},
             ),
-          ),
-          SizedBox(height: 10),
-          RoundedButtonWidget(label: 'Sign in', onPressed: () {}),
-          SizedBox(height: 200),
-        ],
+            SizedBox(height: 30),
+            Text('Or continue with'),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleButtonWidget(
+                  backgroundColor: Color(0xff448AFF),
+                  iconPath: 'assets/icons/facebook.svg',
+                ),
+                SizedBox(width: 10),
+                CircleButtonWidget(backgroundColor: Color(0xffFF1744), iconPath: 'assets/icons/google.svg'),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Don\'t have an account? ', style: TextStyle()),
+                CupertinoButton(
+                  child: Text('Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'sans',
+                        fontWeight: FontWeight.w600,
+                      )),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
